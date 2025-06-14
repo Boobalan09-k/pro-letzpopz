690b42555a125d17ba72461df5b0b34219e0a2d0
Boobalan09-k <boobalan041999@gmail.com>
2025-06-14 15:56:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+  document.getElementById("navLinks").classList.toggle("show");
+}
+
+function showPopup() {
+  document.getElementById("popup").style.display = "block";
+}
+
+function hidePopup() {
+  document.getElementById("popup").style.display = "none";
+}
